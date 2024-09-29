0de383868cd6585ffcdd824ffd1e179cf88ca346
HazratAli <hazratali3711@gmail.com>
2024-09-29 09:36:10 +0600

    feat: added user model, created dtos, implemented authentication

--- server.ts
+++ server.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes";
 
 dotenv.config();
 const app = express();
@@ -8,6 +9,9 @@ const port = process.env.PORT || 8000;
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({ message: "Up and Running" });
 });
+
+app.use(express.json());
+app.use("/api/v1/auth", authRoutes);
 
 mongoose
   .connect(process.env.MONGODB_URI as string)
