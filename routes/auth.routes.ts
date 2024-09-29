3487fc7defeccda3c9711a1f6a657b2b2265fe56
HazratAli <hazratali3711@gmail.com>
2024-09-29 18:36:00 +0600

    feat: added google auth

--- routes/auth.routes.ts
+++ routes/auth.routes.ts
@@ -1,10 +1,21 @@
 import { Router } from "express";
-import { signup, verifyEmail, signin } from "../controllers/auth.controller";
+import {
+  signup,
+  verifyEmail,
+  signin,
+  googleAuth,
+} from "../controllers/auth.controller";
+import passport from "passport";
 
 const router = Router();
 
 router.post("/signup", signup as any);
 router.get("/verify/:token", verifyEmail as any);
 router.post("/signin", signin as any);
+router.get(
+  "/google/callback",
+  passport.authenticate("google", { failureRedirect: "/signin" }),
+  googleAuth as any
+);
 
 export default router;
